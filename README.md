37c0155ce16e998768fda40423f6a71da618850b
Milan Simonovic <mbsimonovic@gmail.com>
2022-03-16 08:47:30 +0100

    add load test script

--- README.md
+++ README.md
@@ -30,11 +30,26 @@ make download-models
 
 To run unit tests:
 ```
-SHARED_ROOT=./shared/installed poetry run coverage run --omit="tests .venv" -m py.test -vv
+make test
 ```
 
+## Load test
 
+First install [k6](https://k6.io/docs/) and then:
 
+```
+cd tools && k6 run --vus 10 -i 5000 questions-k6.js
+# output:
+# default ✓ [======================================] 10 VUs  06m04.3s/10m0s  5000/5000 shared iters
+#     http_reqs......................: 5000    13.724361/s
+#     http_req_duration..............: avg=727.26ms min=78.93ms med=701.05ms max=1.21s  p(90)=812.63ms p(95)=944.71ms
+
+cd tools && k6 run --vus 20 -i 200 questions-k6.js
+# output:
+# default ✓ [======================================] 20 VUs  00m15.3s/10m0s  200/200 shared iters
+     http_reqs......................: 200     13.070371/s
+     http_req_duration..............: avg=1.46s    min=854.79ms med=1.46s max=2.24s  p(90)=1.53s    p(95)=1.55s
+```
 ## Licensing
 
 All source code files must include a USC open license header.
---
+++ tools/encode-k6.js
@@ -0,0 +1,37 @@
+/*
+This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
+
+The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
+*/
+import http from "k6/http";
+import { SharedArray } from "k6/data";
+import { check } from "k6";
+
+const apiUrl = "http://127.0.0.1:5566/v1/encode";
+const questions = new SharedArray("user questions", function () {
+  // subset of https://gist.github.com/khamer/66102c745a9a75de997c038e3166a95d
+  return JSON.parse(open("./questions.json"));
+});
+
+export default function () {
+  // randomly pick one mentor and question:
+  const q = questions[Math.floor(Math.random() * questions.length)];
+  const url = `${apiUrl}?query=${encodeURIComponent(q)}`;
+  const req = http.get(url.toString());
+
+  check(req, {
+    "is status 200": (r) => r.status === 200,
+  });
+  if (req.status === 200) {
+    const res = req.json();
+    // console.log(res.answer_text, q)
+    check(res, {
+      "has no errors": (r) => "errors" in r === false,
+      //	    'has an answer': (r) => r.answer_text && r.answer_text.length > 2,
+    });
+  } else {
+    console.log(req.status, req.text, req.body, q);
+    console.log(url.toString());
+  }
+}
