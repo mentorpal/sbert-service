15002ee4910c67748d5a8296a4bbab3ff7fe15f8
dan100110 <daniel.auerbach@gmail.com>
2022-08-29 15:18:46 -0700

    forgot to save

---
+++ server/api/blueprints/word2vec.py
@@ -0,0 +1,38 @@
+#
+# This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
+#
+# The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
+#
+from dataclasses import dataclass
+import logging
+import json
+from typing import List
+from flask import Blueprint, jsonify, request
+from . import encoder
+from .auth_decorator import authenticate
+
+w2v_blueprint = Blueprint("w2v", __name__)
+
+@dataclass
+class requestBody:
+    words: List[str]
+    model: str
+
+@w2v_blueprint.route("", methods=["GET", "POST"])
+@authenticate
+def encode():
+    if not request.is_json:
+        return (jsonify({"error": "missing json body"}), 400) 
+    json_body = request.get_json()
+    body: requestBody = json.loads(json_body)
+    if body.model is "w2v":
+        return (
+            jsonify(
+                {
+                    "query": sentence,
+                    "encoding": result,
+                }
+            ),
+            200,
+        )
